e23262cdb7ac316cbc909edfb94779871d9d09ba
hoangvu12 <hggaming91@gmail.com>
2023-01-30 16:15:38 +0700

    fix(LocaleChapterSelector): 🐛 show no chapters if no locales

--- src/components/features/manga/LocaleChapterSelector.tsx
+++ src/components/features/manga/LocaleChapterSelector.tsx
@@ -31,45 +31,49 @@ const LocaleChapterSelector: React.FC<LocaleChapterSelectorProps> = ({
 
   return (
     <React.Fragment>
-      <Tabs
-        defaultIndex={defaultTabIndex}
-        selectedTabClassName="bg-white !text-black"
-      >
-        <TabList className="flex items-center justify-end gap-x-1">
-          {localesHasChapters.map(({ locale }, index) => {
-            return (
-              <Tab
-                key={index}
-                className={classNames(
-                  "px-3 py-2 rounded-[6px] cursor-pointer hover:bg-white hover:text-black transition duration-300"
-                )}
-              >
-                {locale.toUpperCase()}
-              </Tab>
-            );
-          })}
-        </TabList>
+      {localesHasChapters?.length ? (
+        <Tabs
+          defaultIndex={defaultTabIndex}
+          selectedTabClassName="bg-white !text-black"
+        >
+          <TabList className="flex items-center justify-end gap-x-1">
+            {localesHasChapters.map(({ locale }, index) => {
+              return (
+                <Tab
+                  key={index}
+                  className={classNames(
+                    "px-3 py-2 rounded-[6px] cursor-pointer hover:bg-white hover:text-black transition duration-300"
+                  )}
+                >
+                  {locale.toUpperCase()}
+                </Tab>
+              );
+            })}
+          </TabList>
 
-        <div className="mt-4">
-          {localesHasChapters.map(({ locale }, index) => {
-            const localeChapters = chapters?.filter((chapter) =>
-              chapter?.source?.locales.some(
-                (sourceLocale) => sourceLocale === locale
-              )
-            );
+          <div className="mt-4">
+            {localesHasChapters.map(({ locale }, index) => {
+              const localeChapters = chapters?.filter((chapter) =>
+                chapter?.source?.locales.some(
+                  (sourceLocale) => sourceLocale === locale
+                )
+              );
 
-            return (
-              <TabPanel key={index}>
-                {!localeChapters?.length ? (
-                  <p className="text-center text-2xl">{t("no_chapters")}</p>
-                ) : (
-                  <ChapterSelector chapters={localeChapters} {...props} />
-                )}
-              </TabPanel>
-            );
-          })}
-        </div>
-      </Tabs>
+              return (
+                <TabPanel key={index}>
+                  {!localeChapters?.length ? (
+                    <p className="text-center text-2xl">{t("no_chapters")}</p>
+                  ) : (
+                    <ChapterSelector chapters={localeChapters} {...props} />
+                  )}
+                </TabPanel>
+              );
+            })}
+          </div>
+        </Tabs>
+      ) : (
+        <p className="text-center text-2xl">{t("no_chapters")}</p>
+      )}
     </React.Fragment>
   );
 };
