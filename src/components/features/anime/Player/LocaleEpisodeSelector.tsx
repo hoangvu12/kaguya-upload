0d5d3a7b73c00c75e057dd08540a43ddbc76e178
hoangvu12 <hggaming91@gmail.com>
2023-01-30 16:15:51 +0700

    fix(LocaleEpisodeSelector): 🐛 show no episodes if no locales

--- src/components/features/anime/Player/LocaleEpisodeSelector.tsx
+++ src/components/features/anime/Player/LocaleEpisodeSelector.tsx
@@ -31,49 +31,53 @@ const LocaleEpisodeSelector: React.FC<SourceEpisodeSelectorProps> = ({
 
   return (
     <React.Fragment>
-      <Tabs
-        defaultIndex={defaultTabIndex}
-        selectedTabClassName="bg-white !text-black"
-      >
-        <TabList className="flex items-center justify-end gap-x-1">
-          {localesHasEpisodes.map(({ locale }) => {
-            return (
-              <Tab
-                key={locale}
-                className={classNames(
-                  "px-3 py-2 rounded-[6px] cursor-pointer hover:bg-white hover:text-black transition duration-300"
-                )}
-              >
-                {locale.toUpperCase()}
-              </Tab>
-            );
-          })}
-        </TabList>
+      {!localesHasEpisodes?.length ? (
+        <Tabs
+          defaultIndex={defaultTabIndex}
+          selectedTabClassName="bg-white !text-black"
+        >
+          <TabList className="flex items-center justify-end gap-x-1">
+            {localesHasEpisodes.map(({ locale }) => {
+              return (
+                <Tab
+                  key={locale}
+                  className={classNames(
+                    "px-3 py-2 rounded-[6px] cursor-pointer hover:bg-white hover:text-black transition duration-300"
+                  )}
+                >
+                  {locale.toUpperCase()}
+                </Tab>
+              );
+            })}
+          </TabList>
 
-        <div className="mt-4">
-          {localesHasEpisodes.map(({ locale }) => {
-            const localeEpisodes = episodes?.filter((episode) =>
-              episode?.source?.locales.some(
-                (sourceLocale) => sourceLocale === locale
-              )
-            );
+          <div className="mt-4">
+            {localesHasEpisodes.map(({ locale }) => {
+              const localeEpisodes = episodes?.filter((episode) =>
+                episode?.source?.locales.some(
+                  (sourceLocale) => sourceLocale === locale
+                )
+              );
 
-            return (
-              <TabPanel key={locale}>
-                {!localeEpisodes?.length ? (
-                  <p className="text-center text-2xl">{t("no_episodes")}</p>
-                ) : (
-                  <SourceEpisodeSelector
-                    key={locale}
-                    episodes={localeEpisodes}
-                    {...props}
-                  />
-                )}
-              </TabPanel>
-            );
-          })}
-        </div>
-      </Tabs>
+              return (
+                <TabPanel key={locale}>
+                  {!localeEpisodes?.length ? (
+                    <p className="text-center text-2xl">{t("no_episodes")}</p>
+                  ) : (
+                    <SourceEpisodeSelector
+                      key={locale}
+                      episodes={localeEpisodes}
+                      {...props}
+                    />
+                  )}
+                </TabPanel>
+              );
+            })}
+          </div>
+        </Tabs>
+      ) : (
+        <p className="text-center text-2xl">{t("no_episodes")}</p>
+      )}
     </React.Fragment>
   );
 };
