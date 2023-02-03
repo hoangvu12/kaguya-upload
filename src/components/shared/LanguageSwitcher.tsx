ba916f3f807246cf8a236e3b2fba8525d2622c42
hoangvu12 <hggaming91@gmail.com>
2023-02-03 22:50:26 +0700

    feat(LanguageSwitcher): 🎸 restyle

--- src/components/shared/LanguageSwitcher.tsx
+++ src/components/shared/LanguageSwitcher.tsx
@@ -2,6 +2,7 @@ import locales from "@/locales.json";
 import { useRouter } from "next/router";
 import nookies from "nookies";
 import React, { useMemo } from "react";
+import { AiOutlineCheck } from "react-icons/ai";
 import { MdOutlineLanguage } from "react-icons/md";
 import Popup from "./Popup";
 
@@ -40,16 +41,21 @@ const LanguageSwitcher = () => {
           </p>
         </div>
       }
+      className="!px-0 w-40"
     >
       <ul className="space-y-1">
         {locales.map(({ locale, name }) => (
           <li
-            className="cursor-pointer transition duration-300 hover:text-primary-300"
+            className="relative px-3 py-2 cursor-pointer transition duration-300 hover:bg-white/20"
             onClick={handleChangeLanguage(locale)}
             key={locale}
             title={locale}
           >
             {name}
+
+            {name === currentLocale.name && (
+              <AiOutlineCheck className="w-5 h-5 absolute right-2 top-1/2 -translate-y-1/2 text-primary-300"></AiOutlineCheck>
+            )}
           </li>
         ))}
       </ul>
