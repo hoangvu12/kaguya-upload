1e6c412911ba9f7520d8381ac798cdb58e335b5d
hoangvu12 <hggaming91@gmail.com>
2023-02-09 22:39:14 +0700

    fix(StickyBanner): 🐛 only set height if script is loaded

--- src/components/features/ads/StickyBanner.tsx
+++ src/components/features/ads/StickyBanner.tsx
@@ -1,12 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
 import classNames from "classnames";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const StickyBanner = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const handleLoaded = () => {
+    console.log("script loaded");
+
+    setIsLoaded(true);
+  };
+
   return (
     <div
       className={classNames(
-        "z-[9000] flex flex-col md:flex-row items-center fixed bottom-4 w-full min-h-[250px]"
+        "z-[9000] flex flex-col md:flex-row items-center fixed bottom-4 w-full",
+        isLoaded && "min-h-[100px]"
       )}
     >
       <div id="pf-63e46763ca8fd80027a851ae">
@@ -15,6 +24,7 @@ const StickyBanner = () => {
           data-cfasync="false"
           src="https://platform.pubfuture.com/v1/unit/63e46763ca8fd80027a851ae.js?v=2"
           type="text/javascript"
+          onLoad={handleLoaded}
         ></script>
       </div>
 
@@ -26,6 +36,7 @@ const StickyBanner = () => {
           data-cfasync="false"
           src="https://platform.pubfuture.com/v1/unit/63e46736a51c1e002988b2d5.js?v=2"
           type="text/javascript"
+          onLoad={handleLoaded}
         ></script>
       </div>
     </div>
