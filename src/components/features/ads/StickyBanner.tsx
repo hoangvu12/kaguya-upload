df8e9349cc62bdf3e5fd793907e3eb8308d8992d
hoangvu12 <hggaming91@gmail.com>
2023-02-13 18:33:31 +0700

    feat: 🎸 add pubfuture's sticky ads

--- src/components/features/ads/StickyBanner.tsx
+++ src/components/features/ads/StickyBanner.tsx
@@ -1,18 +1,28 @@
-/* eslint-disable @next/next/no-img-element */
 import Script from "next/script";
 import React from "react";
 
 const StickyBanner = () => {
   return (
-    <Script id="sticky" type="text/javascript">
-      {`
+    <React.Fragment>
+      <div id="pf-63e46763ca8fd80027a851ae">
+        <Script
+          async
+          data-cfasync="false"
+          src="https://platform.pubfuture.com/v1/unit/63e46763ca8fd80027a851ae.js?v=2"
+          type="text/javascript"
+        ></Script>
+      </div>
+
+      <Script id="sticky" type="text/javascript">
+        {`
         window.googletag = window.googletag || { cmd: [] };
         window.protag = window.protag || { cmd: [] };
         window.protag.cmd.push(function () {
             window.protag.display("protag-sticky-bottom");
         });
       `}
-    </Script>
+      </Script>
+    </React.Fragment>
   );
 };
 
