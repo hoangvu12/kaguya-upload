1003a41ca589b8a7fb64479f9860c00aff6c8a29
hoangvu12 <hggaming91@gmail.com>
2023-02-17 21:56:47 +0700

    feat: 🎸 add 320x50 as sticky banner

--- src/components/features/ads/StickyBanner.tsx
+++ src/components/features/ads/StickyBanner.tsx
@@ -1,47 +1,59 @@
+import CircleButton from "@/components/shared/CircleButton";
 import Script from "next/script";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 
 const StickyBanner = () => {
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     const slots = window.googletag.pubads().getSlots();
+  const [isShow, setIsShow] = useState(true);
 
-  //     for (const slot of slots) {
-  //       if (slot.getSlotElementId() == "protag-sticky-bottom-ad-unit") {
-  //         window.googletag.cmd.push(() => {
-  //           window.googletag.pubads().refresh([slot]);
-  //         });
-  //       }
-  //     }
-  //   }, 30000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const slots = window.googletag.pubads().getSlots();
 
-  //   return () => {
-  //     clearInterval(interval);
-  //   };
-  // }, []);
+      for (const slot of slots) {
+        if (slot.getSlotElementId() == "protag-mobile_leaderboard-ad-unit") {
+          window.googletag.cmd.push(() => {
+            window.googletag.pubads().refresh([slot]);
+          });
+        }
+      }
+    }, 30000);
 
-  // return (
-  //   <React.Fragment>
-  //     <div id="pf-63e46763ca8fd80027a851ae">
-  //       <Script
-  //         async
-  //         data-cfasync="false"
-  //         src="https://platform.pubfuture.com/v1/unit/63e46763ca8fd80027a851ae.js?v=2"
-  //         type="text/javascript"
-  //       ></Script>
-  //     </div>
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
-  //     <Script id="sticky" type="text/javascript">
-  //       {`
-  //       window.googletag = window.googletag || { cmd: [] };
-  //       window.protag = window.protag || { cmd: [] };
-  //       window.protag.cmd.push(function () {
-  //           window.protag.display("protag-sticky-bottom");
-  //       });
-  //     `}
-  //     </Script>
-  //   </React.Fragment>
-  // );
+  const handleClose = () => {
+    setIsShow(false);
+  };
+
+  return (
+    <div className="min-w-[320px] min-h-[50px] fixed bottom-4 left-1/2 -translate-x-1/2 z-[9999]">
+      {isShow && (
+        <div className="w-full h-full" id="protag-mobile_leaderboard"></div>
+      )}
+
+      <CircleButton
+        onClick={handleClose}
+        className="!bg-background-600 absolute -top-5 -right-5"
+        secondary
+        iconClassName="w-4 h-4"
+        LeftIcon={AiOutlineClose}
+        title="Close banner ad"
+      />
+
+      <Script id="sticky" type="text/javascript">
+        {`
+        window.googletag = window.googletag || { cmd: [] };
+        window.protag = window.protag || { cmd: [] };
+        window.protag.cmd.push(function () {
+            window.protag.display("protag-mobile_leaderboard");
+        });
+      `}
+      </Script>
+    </div>
+  );
 
   return null;
 };
