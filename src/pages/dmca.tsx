4c4b031e0a7c63540dbe816876a8344292709de8
hoangvu12 <hggaming91@gmail.com>
2023-03-10 16:01:35 +0700

    fix(DMCAPage): 🐛 rewrite dmca in english

--- src/pages/dmca.tsx
+++ src/pages/dmca.tsx
@@ -1,72 +1,72 @@
 import NativeBanner from "@/components/features/ads/NativeBanner";
 import Head from "@/components/shared/Head";
 
-const dmca = () => {
+const DMCAPage = () => {
   return (
-    <div className="pt-20 px-4 md:px-12 space-y-4">
+    <div className="!max-w-full prose prose-lg prose-invert pt-20 px-4 md:px-12 space-y-4">
       <Head title="DMCA - Kaguya" description="DMCA Kaguya" />
-
       <NativeBanner />
 
-      <h1 className="text-2xl font-bold">Yêu cầu gỡ xuống theo DMCA</h1>
+      <h1 className="text-2xl font-bold">DMCA Takedown Request</h1>
 
       <p>
-        Chúng tôi coi trọng quyền sở hữu trí tuệ của người khác và yêu cầu Người
-        dùng của chúng tôi cũng làm như vậy. Đạo luật Bản quyền Thiên niên kỷ Kỹ
-        thuật số (DMCA) đã thiết lập một quy trình để giải quyết các khiếu nại
-        về vi phạm bản quyền. Nếu bạn sở hữu bản quyền hoặc có quyền thay mặt
-        chủ sở hữu bản quyền và muốn báo cáo khiếu nại rằng bên thứ ba đang vi
-        phạm tài liệu đó, vui lòng gửi báo cáo DMCA trên trang Liên hệ của chúng
-        tôi và chúng tôi sẽ xử lý hành động thích hợp
+        Our anime streaming website provides links to content hosted by
+        third-party sites. We do not host any of the anime or other content
+        ourselves, and we do not have control over the content hosted on these
+        third-party sites. We simply provide links to these sites as a service
+        to our users.
       </p>
-      <p></p>
-      <h4 className="text-xl font-semibold">Yêu cầu đối với Báo cáo DMCA</h4>
-      <ul className="px-4 list-inside list-disc">
-        <li>Mô tả về tác phẩm có bản quyền mà bạn cho rằng đang bị vi phạm;</li>
+      <p>
+        We take copyright infringement very seriously and will promptly remove
+        any content that violates copyright laws or the Digital Millennium
+        Copyright Act (DMCA) when we are notified of such violations. If you
+        believe that any content on our site infringes your copyright or the
+        copyright of someone you represent, please send us a DMCA takedown
+        notice.
+      </p>
+      <p>
+        To file a DMCA takedown notice, please provide us with the following
+        information:
+      </p>
+      <ul>
         <li>
-          Mô tả về tài liệu bạn cho là vi phạm và bạn muốn xóa hoặc quyền truy
-          cập mà bạn muốn vô hiệu hóa và URL hoặc vị trí khác của tài liệu đó;
+          Identification of the copyrighted work you claim has been infringed.
         </li>
+
         <li>
-          Tên, chức danh (nếu là đại lý), địa chỉ, số điện thoại và địa chỉ
-          email của bạn;
+          Identification of the material that you claim is infringing and that
+          you want removed, with enough detail so that we may locate it.
         </li>
+
+        <li>Your name, address, telephone number, and email address.</li>
+
         <li>
-          Một tuyên bố rằng bạn đồng ý với thẩm quyền:{" "}
-          <i>
-            &quot;Tôi thực sự tin tưởng rằng việc sử dụng tài liệu có bản quyền
-            mà tôi đang khiếu nại không được chủ sở hữu bản quyền, người đại
-            diện của chủ sở hữu bản quyền hoặc luật pháp cho phép (ví dụ: sử
-            dụng hợp pháp)&quot;
-          </i>
-          ;
+          A statement that you have a good faith belief that the use of the
+          copyrighted material is not authorized by the copyright owner, its
+          agent, or the law.
         </li>
+
         <li>
-          Một tuyên bố rằng bạn không khai man:{" "}
-          <i>
-            &quot;Thông tin trong thông báo này là chính xác và, dưới hình phạt
-            khai man, tôi là chủ sở hữu, hoặc được ủy quyền hành động thay mặt
-            cho chủ sở hữu, bản quyền hoặc quyền độc quyền bị cáo buộc vi
-            phạm&quot;
-          </i>
-          ;
-        </li>
-        <li>
-          Chữ ký điện tử hoặc vật lý của chủ sở hữu bản quyền hoặc người được ủy
-          quyền hành động thay mặt chủ sở hữu.
+          A statement that the information in your notice is accurate and that
+          you are the copyright owner or authorized to act on the copyright
+          owner&apos;s behalf.
         </li>
       </ul>
-      <p></p>
       <p>
-        Yêu cầu gỡ xuống DMCA của bạn phải được gửi tới email:{" "}
-        hggaming07@gmail.com
+        Please send your DMCA takedown notice to{" "}
+        <a
+          target={"_blank"}
+          href="mailto:hggaming07@gmail.com"
+          rel="noreferrer"
+        >
+          <b>hggaming07@gmail.com</b>
+        </a>
+        . We will promptly investigate and take appropriate action in accordance
+        with the DMCA.
       </p>
-      <p>
-        Sau đó, chúng tôi sẽ xem xét yêu cầu DMCA của bạn và thực hiện các hành
-        động thích hợp, bao gồm cả việc xóa nội dung khỏi trang web.
-      </p>
+      <p>Thank you for your cooperation.</p>
     </div>
   );
 };
 
-export default dmca;
+export default DMCAPage;
