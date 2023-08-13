e6a27bb5a01ae9423aa050dba5df8406bd84b39f
hoangvu12 <hggaming91@gmail.com>
2023-08-13 22:55:37 +0700

    feat: add sources api

---
+++ src/pages/api/source/anime.ts
@@ -0,0 +1,28 @@
+import { supabaseClient } from "@supabase/auth-helpers-nextjs";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "GET") {
+    return res
+      .status(405)
+      .json({ success: false, errorMessage: "Method not allowed" });
+  }
+
+  const { data: sources, error: connectionError } = await supabaseClient
+    .from("kaguya_sources")
+    .select("languages, id, name")
+    .contains("mediaType", ["ANIME"]);
+
+  if (connectionError) {
+    return res
+      .status(500)
+      .json({ success: false, errorMessage: connectionError.message });
+  }
+
+  res.status(200).json({
+    success: true,
+    sources,
+  });
+};
+
+export default handler;
